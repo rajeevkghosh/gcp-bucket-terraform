6a03f8cb1e2573f392576c06ec0cbc0389daa851
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-09-21 18:03:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  access_token = var.access_token
+}
+
+resource "google_storage_bucket" "gcp_bucket" {
+  project      = "tokyo-nomad-323506"
+  name          = "gcp_bucket_terraform_rajeev"
+  location      = "US"
+  force_destroy = true
+
+  labels = {
+      env        =  "dev"
+      foo        =  "bar"
+    }
+}
